a5d629fa6f7f0b41e61927ffe5aa2efdc37c6918
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-11-28 18:34:29 -0800

    uniteUnique completed.

---
+++ uniteUnique/main.js
@@ -0,0 +1,23 @@
+// uniteUnique takes two or more arrays as parameter,
+// returns a new array of unique vaues in the order of the
+// original provided arrays.
+// Create an empty array variable,
+// Use arguments object then loop through each argument,
+// then push the result.
+
+function uniteUnique(...array) {
+  const result = []
+  for(let i = 0; i < arguments.length; i++) {
+    const newArray = arguments[i]
+    for(let j = 0; j < newArray.length; j++) {
+      const index = newArray[j]
+      if(result.indexOf(index) < 0) {
+        result.push(index)
+      }
+    }
+  }
+  return result
+}
+
+console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]))
+console.log(uniteUnique([1, 3, 2], [1, [5]], [2, [4]]))
