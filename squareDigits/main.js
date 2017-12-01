655850b14c633c0f9e3e6a51e210ac059d7faa7f
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-11-30 20:44:50 -0800

    Comment removed.

--- squareDigits/main.js
+++ squareDigits/main.js
@@ -1,6 +1,3 @@
-// squareDigits takes a number and returns
-// a string each number squared.
-
 function squareDigits(number) {
   const splitNumber = number.toString().split('')
   return parseInt(splitNumber.map(number => Math.pow(number, 2)).join(''))
