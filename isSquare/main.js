0cab6e8ec45ec3d5db0501b72f7cffc9c2891b0f
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-11-30 17:56:12 -0800

    Removed comments.

--- isSquare/main.js
+++ isSquare/main.js
@@ -1,7 +1,3 @@
-// isSquare takes number, returns true if number is
-// square, false if it isn't.
-// Use Math.sqrt and if it can be devided by 1 true, else false.
-
 function isSquare(number) {
   if (Math.sqrt(number) % 1 === 0) {
     return true
