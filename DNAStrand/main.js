0be2d5229ffb58f2a6145bb9dffd6ea3cf457dd8
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-11-30 20:25:31 -0800

    DNAStrand completed.

---
+++ DNAStrand/main.js
@@ -0,0 +1,26 @@
+// DNAStrand takes a string, return a string with
+// complementary DNA to each letter.
+
+function DNAStrand(string) {
+  let result = ''
+  const split = string.split('')
+  for(let i = 0; i < split.length; i++) {
+    switch(split[i]) {
+      case 'A':
+        result += 'T'
+        break
+      case 'T':
+        result += 'A'
+        break
+      case 'G':
+        result += 'C'
+        break
+      case 'C':
+        result += 'G'
+    }
+  }
+  return result
+}
+
+console.log(DNAStrand('AAAA'));
+console.log(DNAStrand("ATTGC"));
