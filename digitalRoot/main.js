cc6f89989ebc04a513d8cd46f5ebfc58ac625b2b
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-11-30 18:18:52 -0800

    comment removed.

--- digitalRoot/main.js
+++ digitalRoot/main.js
@@ -1,8 +1,3 @@
-// digitalRoot takes number, if the number is more than two digits,
-// divide number into single digit and add. Keep reducing til
-// it becomes a single digit.
-// split the number into array, use reduce to keep adding.
-
 function digitalRoot(number) {
   let sum = 0
   const split = number.toString().split('')
