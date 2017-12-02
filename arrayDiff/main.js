251185671cbaeca1ccc952b29edb647dc619a9bb
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-12-01 17:46:52 -0800

    arrayDiff test passed.

---
+++ arrayDiff/main.js
@@ -0,0 +1,21 @@
+// arrayDiff takes two arrays,
+// subtracts one list from another and return.
+// create empty array variable, loop thru first array,
+// check if array2 isnt in array1 then push array1 index.
+
+function arrayDiff(array1, array2) {
+  const result = []
+  for(let i = 0; i < array1.length; i++) {
+    if(array1[i] !== array2[0]) {
+      result.push(array1[i])
+    }
+  }
+  return result
+}
+
+function arrayDiff2(array1, array2) {
+  return array1.filter(item => array2.indexOf(item) == -1)
+}
+
+console.log(arrayDiff([1,2,2,2,3],[2]));
+console.log(arrayDiff2([1,2],[1]));
