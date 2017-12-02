5eefe1f1156be29c8ed8a2e3b2c15eb5d4447fc3
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-12-02 13:39:01 -0800

    comments removed.

--- uniteUnique/main.js
+++ uniteUnique/main.js
@@ -4,20 +4,35 @@
 // Create an empty array variable,
 // Use arguments object then loop through each argument,
 // then push the result.
+//
+// function uniteUnique(...array) {
+//   const result = []
+//   for(let i = 0; i < arguments.length; i++) {
+//     const newArray = arguments[i]
+//     for(let j = 0; j < newArray.length; j++) {
+//       const index = newArray[j]
+//       if(result.indexOf(index) < 0) {
+//         result.push(index)
+//       }
+//     }
+//   }
+//   return result
+// }
+//
+// console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]))
+// console.log(uniteUnique([1, 3, 2], [1, [5]], [2, [4]]))
 
-function uniteUnique(...array) {
-  const result = []
-  for(let i = 0; i < arguments.length; i++) {
-    const newArray = arguments[i]
-    for(let j = 0; j < newArray.length; j++) {
-      const index = newArray[j]
-      if(result.indexOf(index) < 0) {
-        result.push(index)
-      }
-    }
-  }
-  return result
+function counterEffect(hitCount) {
+ const split = hitCount.split('')
+ const result = []
+ for(let i = 0; i < split.length; i++) {
+   const temp = []
+   for(let j = 0; j <= Number(split[i]); j++) {
+     temp.push(j)
+   }
+   result.push(temp)
+ }
+ return result
 }
 
-console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]))
-console.log(uniteUnique([1, 3, 2], [1, [5]], [2, [4]]))
+console.log(counterEffect("1250"));
