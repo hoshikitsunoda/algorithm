d46de5bf4c64304f22c9ab116fcac3df910ae669
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-12-02 10:15:28 -0800

    validParentheses completed.

--- validParentheses/main.js
+++ validParentheses/main.js
@@ -1,8 +1,3 @@
-// validParentheses takes string of Parentheses,
-// checks if the order is valid and returns boolean.
-// Divide initial string by 2, replace each with empty string,
-// if result matches to empty string return true.
-
 function validParentheses(string) {
   const divided = string.length / 2
   for(let i = 0; i < divided; i++) {
