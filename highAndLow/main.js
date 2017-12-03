4feca32e297525370c135d51e43ab108a7e3014c
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-12-03 09:56:44 -0800

    highAndLow completed.

--- highAndLow/main.js
+++ highAndLow/main.js
@@ -1,8 +1,3 @@
-// highAndLow takes a string of numbers and returns
-// highest and lowest number.
-// Split the string, sort in ascending order,
-// return them in array then join with space.
-
 function highAndLow(string) {
   const sorted = string.split(' ').sort((a, b) => b - a)
   return [sorted[0], sorted[sorted.length -1]].join(' ')
