b14bcd2b577f27c8e896388738f1593886b9e9e3
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-12-03 10:10:44 -0800

    getSum completed.

--- getSum/main.js
+++ getSum/main.js
@@ -1,8 +1,3 @@
-// getSum takes two integers and returns sum of all number inbetween.
-// if numbers are the same, return either.
-// Make max and min variable with Math.max/min, and sum set to 0,
-// while min is smaller than max add min to sum, return.
-
 function getSum(a, b) {
   const max = Math.max(a, b)
   let min = Math.min(a, b)
