ca2b5de75614ea4567db946d0ebc04e71ece3528
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-12-04 09:02:40 -0800

    inArray test passed.

---
+++ inArray/main.js
@@ -0,0 +1,25 @@
+// inArray takes two arrays, returns a new sorted array
+// in lexicographical order of the strings of a1 which are
+// substrings of strings of a2.
+
+function inArray(array1, array2) {
+  var result = [];
+
+  outer: for (j = 0; j < array1.length; j++) {
+      for (i = 0; i < array2.length; i++) {
+          if (array2[i].includes(array1[j])) {
+              result.push(array1[j]);
+              continue outer;
+          }
+      }
+  }
+  return result.sort();
+}
+
+function inArray2(array1,array2){
+  return array1
+    .filter(a1 => array2.find(a2 => a2.match(a1)))
+    .sort()
+}
+
+console.log(inArray(["xyz", "live", "strong"], ["lively", "alive", "harp", "sharp", "armstrong"]));
