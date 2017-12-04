f9a242a8c6462c8fa5e6c449433c03bd2b2aa93f
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-12-04 09:03:10 -0800

    inArray completed.

--- inArray/main.js
+++ inArray/main.js
@@ -1,7 +1,3 @@
-// inArray takes two arrays, returns a new sorted array
-// in lexicographical order of the strings of a1 which are
-// substrings of strings of a2.
-
 function inArray(array1, array2) {
   var result = [];
 
