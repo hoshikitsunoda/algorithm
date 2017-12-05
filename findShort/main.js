9d3b1b35cc90c8786bef3ae5986c0e76f1270389
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-12-05 01:22:16 -0800

    findShort completed.

--- findShort/main.js
+++ findShort/main.js
@@ -1,6 +1,3 @@
-// findShort takes a strong and returns length of the shortest word.
-// split, sort and length of first item.
-
 function findShort(string) {
   return string.split(' ').sort((x, y) => x.length - y.length)[0].length
 }
