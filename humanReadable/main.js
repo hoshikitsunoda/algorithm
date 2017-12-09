51637aabba79299035ea13925f49c2290d8d5679
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-12-08 23:51:56 -0800

    humanReadable completed.

--- humanReadable/main.js
+++ humanReadable/main.js
@@ -1,9 +1,3 @@
-// humanReadable takes number and returns the time in
-// a human-readable format(HH:MM:SS).
-// if input is 0, returns 00:00:00
-// if input is 60, returns 00:01:00
-// if input is 86399, returns 23:59:59
-
 function humanReadable(seconds) {
   const pad = (x) => (x < 10) ? "0" + x : x
   return pad(parseInt(seconds / (60 * 60))) + ":" +
