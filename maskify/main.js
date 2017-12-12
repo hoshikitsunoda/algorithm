e3255dc862d1d0143972b6d4716693763d524ef1
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-12-11 17:36:44 -0800

    maskify completed.

--- maskify/main.js
+++ maskify/main.js
@@ -1,12 +1,7 @@
-// maskify takes a string and returns a new string with
-// all letters excepts last 4 replaced with #
-
 function maskify(string) {
   if(string.length < 4) return string
   return string.split('').slice(0, string.length - 4).join('').replace(/./g, '#') + string.substring(string.length - 4)
 }
-
-// simplified version
 
 function maskify2(cc) {
   return cc.slice(0, -4).replace(/./g, '#') + cc.slice(-4);
