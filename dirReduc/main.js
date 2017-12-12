eb986d02a3b2d79cf624832d7a72449ee1d53ea3
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-12-11 17:24:56 -0800

    dirReduc completed.

--- dirReduc/main.js
+++ dirReduc/main.js
@@ -1,7 +1,3 @@
-// dirReduc takes set of directions(arry or object),
-// returns  an array of strings with the needless
-// directions removed (W<->E or S<->N side by side).
-
 function dirReduc(array) {
   let i = 0
   while(i < array.length) {
