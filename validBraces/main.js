9061a8cf51efbf083d26f4b2edfafd0e7adc8f7b
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-12-15 23:56:32 -0800

    validBraces completed.

--- validBraces/main.js
+++ validBraces/main.js
@@ -1,7 +1,3 @@
-// Write a function that takes a string of braces,
-// and determines if the order of the braces is valid.
-// It should return true if the string is valid, and false if it's invalid.
-
 function validBraces(string) {
   const regex = /\(\)|\[\]|\{\}/g
 
