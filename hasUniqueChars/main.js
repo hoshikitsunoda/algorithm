1c82ac17ec7de79f7630397858ea0930406495cd
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-12-17 20:38:11 -0800

    hasUniqueChars completed.

--- hasUniqueChars/main.js
+++ hasUniqueChars/main.js
@@ -1,6 +1,3 @@
-// hasUniqueChars takes a string, checks if a string contains
-// all unique characters, returns true if it does, false otherwise.
-
 function hasUniqueChars(string) {
   return string.split('').every((a, b, array) => {
     return array.indexOf(a) == b
