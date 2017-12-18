42f0bcbb039a8cbaf24d1486488b91f2156d88b0
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-12-17 20:18:36 -0800

    getElement test passed.

---
+++ getElement/main.js
@@ -0,0 +1,12 @@
+// getElement takes an array and return an element from
+// the multidimensional array (length > 0) by the specified
+// indices (length > 0).
+
+function getElement(array, index) {
+  return index.reduce((accumulator, current) => {
+    return accumulator[current]
+  }, array)
+}
+
+console.log(getElement([[1, 2], [3, 4], [5, 6]], [0, 0]));
+console.log(getElement([[[ 1, 2, 3]]], [ 0, 0, 1 ]));
