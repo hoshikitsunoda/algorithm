97fb51df02fa37d5244c985235f013481063d8a9
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-12-17 19:53:50 -0800

    swapHeadAndTail test passed.

--- swapHeadAndTail/main.js
+++ swapHeadAndTail/main.js
@@ -4,7 +4,9 @@
 // The middle element (if it exists) leaves on the same position.
 
 function swapHeadAndTail(array) {
-  return array.splice(0, 2)
+  if (array.length == 1) return array
+  var n = Math.floor(array.length / 2)
+  return array.slice(-n).concat((n * 2 == array.length ? [] : array.slice(n, n + 1)), array.slice(0, n))
 }
 
 console.log(swapHeadAndTail([ 1, 2, 3, 4, 5 ]));
