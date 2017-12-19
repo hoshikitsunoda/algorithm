74e5f2a970ada9d694c054ad4f36421fe279863d
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-12-18 16:26:32 -0800

    arithmeticSequenceElements completed.

--- arithmeticSequenceElements/main.js
+++ arithmeticSequenceElements/main.js
@@ -1,7 +1,3 @@
-// write a function that will return the first 'n' elements
-// of the sequence with the given common difference 'r' and
-// first element 'a'. Result should be separated by comma and space.
-
 function arithmeticSequenceElements(a, r, n) {
   let array = []
   for(let i = 0; i < n; i++) {
