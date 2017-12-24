0f52fa95fedecc763416888b9278ae64e7a7700d
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-12-23 22:09:41 -0800

    evenOrOdd test passed.

---
+++ evenOrOdd/main.js
@@ -0,0 +1,8 @@
+// Create a function that takes an integer as an argument
+// and returns "Even" for even numbers or "Odd" for odd numbers.
+
+function evenOrOdd(number) {
+  return (number % 2 === 0) ? 'Even' : 'Odd'
+}
+
+console.log(evenOrOdd(2));
