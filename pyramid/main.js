7dfb37f68ba7e297def173363755d7fa1f0e33c7
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-12-26 21:30:34 -0800

    pyramid test passed.

---
+++ pyramid/main.js
@@ -0,0 +1,19 @@
+// Given a number n you print a pyramid with n floors.
+
+function pyramid(number) {
+  let result = ""
+  for(let i = 0; i < number; i++) {
+    for(let j = 0; j < number-i-1; j++) {
+      result += " "
+    }
+    result += "/"
+    for(let j=0; j<i; j++) {
+      if (i == number-1) result += "__"
+      else result += "  "
+    }
+    result += "\\\n"
+  }
+  return result;
+}
+
+console.log(pyramid(10));
