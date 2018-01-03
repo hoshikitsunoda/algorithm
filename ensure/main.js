25a95434fcbed9b2625dfd2a8bc736935739b609
Hoshiki Tsunoda <liquidohr@gmail.com>
2018-01-03 00:12:02 -0800

    ensure completed.

--- ensure/main.js
+++ ensure/main.js
@@ -1,6 +1,3 @@
-// Implement the ensure function so that it throws an error if called without
-// arguments or an argument is undefined. Otherwise it should return the given value.
-
 function ensure(value) {
   return value === undefined ? 'error' : value
 }
