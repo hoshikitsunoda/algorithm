b199ed8357569ae10f0ee9ee398100b987985262
Hoshiki Tsunoda <liquidohr@gmail.com>
2018-01-03 14:18:43 -0800

    removeProperty completed.

--- removeProperty/main.js
+++ removeProperty/main.js
@@ -1,6 +1,3 @@
-// If the object obj has a property prop, the function removes the property
-// from the object and returns true; in all other cases it returns false.
-
 function removeProperty(obj, prop) {
   if(prop in obj) {
     delete obj[prop]
