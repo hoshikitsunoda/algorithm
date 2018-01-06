f5c56db762b221b44d3c497652b320adf8c27aab
Hoshiki Tsunoda <liquidohr@gmail.com>
2018-01-05 19:27:17 -0800

    reverser completed.

--- reverser/main.js
+++ reverser/main.js
@@ -1,6 +1,3 @@
-// Take a sentence (string) and reverse each letter in each word in the sentence.
-// Do not reverse the order of the words, just the letters in each word.
-
 function reverser(string) {
   const split = string.split(' ')
   const reversed = split.map(word => {
