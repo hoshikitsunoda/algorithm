747663aa900c4cb818dae184f7ab9c1241657236
Hoshiki Tsunoda <liquidohr@gmail.com>
2018-01-06 20:20:20 -0800

    isIsogram completed.

--- isIsogram/main.js
+++ isIsogram/main.js
@@ -1,6 +1,3 @@
-// Implement a function that determines whether a string that contains only letters is an isogram.
-// An isogram is a word that has no repeating letters, consecutive or non-consecutive.
-
 function isIsogram(string) {
   const regex = /(\w).*\1/i
   return !regex.test(string)
