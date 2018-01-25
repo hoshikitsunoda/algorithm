36d596d4bb99bac94683f089644759d79fe4920e
Hoshiki Tsunoda <liquidohr@gmail.com>
2018-01-24 22:47:34 -0800

    openOrSenior completed.

--- openOrSenior/main.js
+++ openOrSenior/main.js
@@ -1,6 +1,3 @@
-// To be a senior, a member must be at least 55 years old and have a handicap greater than 7.
-// In this croquet club, handicaps range from -2 to +26; the better the player the lower the handicap.
-
 function openOrSenior(data){
   const result = []
   data.forEach(member => {
