998c07e93bee4a72a56b3cd37bc1e862b1783988
seiki tsunoda <liquidohr@gmail.com>
2018-02-16 18:44:39 -0800

    titleCase finished

---
+++ titleCase/index.js
@@ -0,0 +1,7 @@
+function titleCase(string) {
+  return string.toLowerCase().split(' ').map((word) => {
+    return (word.charAt(0).toUpperCase() + word.slice(1))
+  }).join(' ')
+}
+
+module.exports = titleCase
