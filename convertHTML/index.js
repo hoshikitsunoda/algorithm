ab8b0ab716013e870ea221e3feb008574c517b78
seiki tsunoda <liquidohr@gmail.com>
2018-03-09 12:23:56 -0800

    advanced solution tested

--- convertHTML/index.js
+++ convertHTML/index.js
@@ -1,42 +1,18 @@
 function convertHTML(str) {
-  const split = str.split('')
-  for(let i = 0; i < split.length; i++) {
-    switch (split[i]) {
-      case '<':
-        split[i] = '&lt;';
-        break;
-      case '&':
-        split[i] = '&amp;';
-        break;
-      case '>':
-        split[i] = '&gt;';
-        break;
-      case '"':
-        split[i] = '&quot;';
-        break;
-      case "'":
-        split[i] = "&apos;";
-        break;
-    }
+  var entities = {
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '"': '&quot;',
+    '\'': '&apos;'
+  };
+  for(var i in entities) {
+    var before = i;
+    var after = entities[i];
+    var pattern = new RegExp(before, 'g');
+    str = str.replace(pattern, after);
   }
-  return split.join('')
+  return str;
 }
-
-// function convertHTML(str) {
-//   var entities = {
-//     '&': '&amp;',
-//     '<': '&lt;',
-//     '>': '&gt;',
-//     '"': '&quot;',
-//     '\'': '&apos;'
-//   };
-//   for(var i in entities) {
-//     var before = i;
-//     var after = entities[i];
-//     var pattern = new RegExp(before, 'g');
-//     str = str.replace(pattern, after);
-//   }
-//   return str;
-// }
 
 module.exports = convertHTML
