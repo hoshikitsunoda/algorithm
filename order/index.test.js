d8c4f3963bc146b6e66e2bbe26e9dc69212de473
seiki tsunoda <liquidohr@gmail.com>
2018-04-17 07:27:18 -0700

    test passed

--- order/index.test.js
+++ order/index.test.js
@@ -2,7 +2,13 @@ const order = require('./index.js')
 const { expect } = require('chai')
 
 describe('sort order', () => {
-  it("sorts the string", () => {
+  it("returns sorted string", () => {
     expect(order("is2 Thi1s T4est 3a")).to.equal("Thi1s is2 3a T4est")
   })
 })
+
+describe('sort order', () => {
+    it("returns empty string", () => {
+      expect(order("")).to.equal("")
+    })
+  })
