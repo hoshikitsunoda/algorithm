36f71ffdda73b189df77d7b07ca480b3b5104d34
seiki tsunoda <liquidohr@gmail.com>
2018-04-25 21:41:34 -0700

    duplicateCount test passed

--- duplicateCount/index.test.js
+++ duplicateCount/index.test.js
@@ -12,3 +12,9 @@ describe('prints the number of duplicate value', () => {
         expect(duplicateCount("aabbcde")).to.equal(2)
     })
 })
+
+describe('prints the number of duplicate value', () => {
+    it('prints 1 because i is duplicate', () => {
+        expect(duplicateCount("Indivisibility")).to.equal(1)
+    })
+})
