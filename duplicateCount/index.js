7f08468d7ffb43e447f62dc7490f6e5c3ff1b605
seiki tsunoda <liquidohr@gmail.com>
2018-04-25 21:41:21 -0700

    duplicateCount completed

--- duplicateCount/index.js
+++ duplicateCount/index.js
@@ -1,14 +1,12 @@
 const duplicateCount = (string) => {
     const split = string.toLowerCase().split('')
-    const duplicates = []
-    for(let i = 0; i < split.length; i++) {
-        console.log(split[i])
-        if(split[i] === split[i - 1]) {
-            console.log(split[i], split[i - 1])
-            duplicates.push(split[i])
-        }
-    }
-    return duplicates.length
+    const duplicates = split.filter((a, b) => {
+        console.log(split.indexOf(a))
+        return split.indexOf(a) !== b
+    })
+    return duplicates.filter((letter, position) => {
+        return duplicates.indexOf(letter) === position
+    }).length
 }
 
 module.exports = duplicateCount
