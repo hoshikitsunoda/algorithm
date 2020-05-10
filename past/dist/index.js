bdc8a52023214ca845d8a6c95db46221afd0a695
Hosh'ki Tsunoda <hoshkitsunoda@gmail.com>
2020-05-11 02:04:14 +0900

    moved all TypeScript katas to TypeScript folder

--- past/dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function past(h, m, s) {
-    h = h * 60 * 60 * 1000;
-    m = m * 60 * 1000;
-    s = s * 1000;
-    return +(h + m + s);
-}
-console.log(past(1, 1, 1));
-//# sourceMappingURL=index.js.map
