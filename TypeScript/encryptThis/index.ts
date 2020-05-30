61c589646c03ff0b4e43ffd6259b6ea5a623e9d6
Hosh'ki Tsunoda <hoshkitsunoda@gmail.com>
2020-05-31 01:59:51 +0900

    completed encryptThis

---
+++ TypeScript/encryptThis/index.ts
@@ -0,0 +1,18 @@
+const encryptThis = (str: string): string => {
+  if (str === '') return ''
+  const splitted: string[] = str.split(' ')
+  return splitted
+    .map((item: string) => {
+      const firstLetterAscii: number = item.substring(0, 1).charCodeAt(0)
+      const split: any = item.substring(1).split('')
+      let temp: number = 0
+      temp = split[0]
+      split[0] = split[split.length - 1]
+      split[split.length - 1] = temp
+      item = firstLetterAscii + split.join('')
+      return item
+    })
+    .join(' ')
+}
+
+console.log(encryptThis('A wise old owl lived in an oak'))
