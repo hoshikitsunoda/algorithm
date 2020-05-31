f2a7ce86c182898b69a552a6a287d288afb7d618
Hosh'ki Tsunoda <hoshkitsunoda@gmail.com>
2020-05-31 17:37:30 +0900

    added js-boilerplate

---
+++ JavaScript/js-boilerplate/index.js
@@ -0,0 +1,3 @@
+const functionName = () => {}
+
+module.exports = functionName
---
+++ JavaScript/js-boilerplate/index.test.js
@@ -0,0 +1,8 @@
+const functionName = require('./index')
+const { expect } = require('chai')
+
+describe('test description', () => {
+  it('does something', () => {
+    expect(functionName()).to.eql()
+  })
+})
