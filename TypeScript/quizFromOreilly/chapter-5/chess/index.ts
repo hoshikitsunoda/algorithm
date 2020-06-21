df453b1591725f553077af9435001310814b247f
Hosh'ki Tsunoda <hoshkitsunoda@gmail.com>
2020-06-21 23:36:14 +0900

    chess excersize in lesson 5.1  completed

---
+++ TypeScript/quizFromOreilly/chapter-5/chess/index.ts
@@ -0,0 +1,62 @@
+type Color = 'Black' | 'White'
+
+type BoardFile = 'A' | 'B' | 'C' | 'D' | 'E' | 'F' | 'G' | 'H'
+
+type BoardRank = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8
+
+class Game {
+  private pieces = Game.makePieces()
+
+  private static makePieces() {
+    return [new King('White', 'E', 1)]
+  }
+
+  showPieces() {
+    return this.pieces
+  }
+}
+
+abstract class Piece {
+  protected position: Position
+  constructor(private readonly color: Color, file: BoardFile, rank: BoardRank) {
+    this.position = new Position(file, rank)
+    this.color
+  }
+
+  moveTo(position: Position) {
+    this.position = position
+  }
+
+  abstract canMoveTo(position: Position): boolean
+}
+
+class Position {
+  constructor(private file: BoardFile, private rank: BoardRank) {}
+
+  distanceFrom(position: Position) {
+    return {
+      rank: Math.abs(position.rank - this.rank),
+      file: Math.abs(position.file.charCodeAt(0) - this.file.charCodeAt(0)),
+    }
+  }
+}
+
+class King extends Piece {
+  canMoveTo(position: Position) {
+    let distance = this.position.distanceFrom(position)
+    return distance.rank < 2 && distance.file < 2
+  }
+}
+
+const kingPin = new Game()
+console.log(kingPin)
+
+// class Queen extends Piece {}
+
+// class Bishop extends Piece {}
+
+// class Knight extends Piece {}
+
+// class Rook extends Piece {}
+
+// class Pawn extends Piece {}
