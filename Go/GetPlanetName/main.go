f7af4d1223f2defc575888df786cbc230f2b487b
Hosh'ki Tsunoda <hoshkitsunoda@gmail.com>
2020-08-07 01:55:43 -0700

    getPlanetName completed

---
+++ Go/GetPlanetName/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+)
+
+func getPlanetName(ID int) string {
+	switch ID {
+	case 1:
+		return "Mercury"
+	case 2:
+		return "Venus"
+	case 3:
+		return "Earth"
+	case 4:
+		return "Mars"
+	case 5:
+		return "Jupiter"
+	case 6:
+		return "Saturn"
+	case 7:
+		return "Uranus"
+	case 8:
+		return "Neptune"
+	default:
+		return "Pluto"
+	}
+}
+
+func main() {
+	fmt.Print(getPlanetName(2))
+}
